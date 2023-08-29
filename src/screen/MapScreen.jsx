ddb3453ab6bd42a5e68ede3232c1cd0d10e790a2
Udonwajnr <udonwajnr10@gmail.com>
2023-08-29 21:29:37 +0100

    commit

--- src/screen/MapScreen.jsx
+++ src/screen/MapScreen.jsx
@@ -1,7 +1,17 @@
-import { Text } from "react-native"
+import { Text,View,Platform,Linking, Button } from "react-native"
 
 export default function MapScreen(){
+    const openGoogleMap = () => {
+        const scheme = Platform.select({ ios: 'maps:0,0?q=', android: 'geo:0,0?q=' });
+        const latLng = `${4.868680631309251},${ 6.989942368161841}`;
+        const label = 'Palkeper';
+        const url = Platform.select({
+          ios: `${scheme}${label}@${latLng}`,
+          android: `${scheme}${latLng}(${label})`,
+        });
+        Linking.openURL(url);
+      };
     return(
-        <Text>This is the Map Screen</Text>
+        <Button title="View Map" onPress={()=>openGoogleMap()}/>
     )
 }
